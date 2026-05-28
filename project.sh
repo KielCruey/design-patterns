e135d0c1fe9e879916adcb1c1c95d78abebea048
Kiel Cruey <kscruey@gmail.com>
2026-05-28 02:31:34 -0700

    updated project script

--- project.sh
+++ project.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
 # deletes all temp directories/files
+echo DELETING BUILD AND DOXYGEN FILES
 rm -fr cppcheck-report.txt build doxygen
 
 # cppcheck -- static analysis
+echo RUNNING CPP-CHECK
 cppcheck --suppress=missingIncludeSystem --error-exitcode=1 --enable=all --output-file=cppcheck-report.txt .
 
 # doxygen -- documentation
+echo RUNNING DOXYDEN
 doxygen Doxyfile
 
 # cmake -- builds project
+echo BUILDING USING CMAKE
 cmake -G "MinGW Makefiles" -B build -S .
 cmake --build build
+
+# clang-format -- formatting all code
+echo RUNNING CLANG-FORMAT
+find . -iname "*.cpp" -o -iname "*.hpp" -o -iname "*.inl" | xargs clang-format -i
+
+echo SCRIPT COMPLETED
